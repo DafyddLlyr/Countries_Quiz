66eb61aded1e02f927c71316c2ee35533bafdd2d
Laura Sheach <laura_bubble_git@yahoo.com>
2019-06-30 20:31:28 +0100

    Drop down menu of countries now available and eventBus set up for selected country

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -3,6 +3,11 @@
     <h3>Hello, {{user.name}}!</h3>
     <button v-on:click="handleShowTopics" type="button" name="button">Select Quiz</button>
     <br>
+    <label for="country">Pick a country to explore:</label>
+    <select id="country-picked" v-model="countryChoice" name="country" @change="handleCountryChoice">
+      <option v-for="(country, index) in countryData" :country="country" :key="index" :value="country">{{country.name}}</option>
+    </select>
+    <br>
     <button v-on:click="handleLogOut" type="button" name="button">Log out</button>
   </div>
 
@@ -13,14 +18,31 @@ import {eventBus} from '../main.js'
 
 export default {
   name: 'nav-bar',
+  data(){
+    return {
+      'countryData': [],
+      'countryChoice': {}
+    }
+  },
   props: ['user'],
+  mounted(){
+    this.fetchCountryData()
+  },
   methods: {
     handleLogOut(){
       eventBus.$emit('log-out')
     },
     handleShowTopics(){
       eventBus.$emit('show-topics')
-    }
+    },
+    handleCountryChoice(){
+      eventBus.$emit('country-choice', this.countryChoice)
+    },
+    fetchCountryData(){
+      fetch('https://restcountries.eu/rest/v2/all')
+      .then( res => res.json())
+      .then( result => this.countryData = result)
+    },
   }
 }
 
