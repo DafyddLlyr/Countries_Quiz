1754e86989f06b2ecf5b0eed6d87a61edf160b48
dafyddllyr <dafyddllyr@gmail.com>
2019-06-30 17:07:12 +0100

    Basic globe added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+
     <landing-page v-if="!user"/>
 
     <div id="profile-container" v-if="user">
@@ -13,56 +14,56 @@
 </template>
 
 <script>
-import {eventBus} from './main.js'
+  import {eventBus} from './main.js'
 
-import LandingPage from './components/LandingPage.vue'
-import ProfileHeader from './components/ProfileHeader.vue'
-import NavBar from './components/NavBar.vue'
-import MapOverlay from './components/MapOverlay.vue'
+  import LandingPage from './components/LandingPage.vue'
+  import ProfileHeader from './components/ProfileHeader.vue'
+  import NavBar from './components/NavBar.vue'
+  import MapOverlay from './components/MapOverlay.vue'
 
-export default {
-  name: 'app',
-  data(){
-    return {
-      'user': null
+  export default {
+    name: 'app',
+    data(){
+      return {
+        'user': null
+      }
+    },
+    components: {
+      'landing-page': LandingPage,
+      'profile-header': ProfileHeader,
+      'nav-bar': NavBar,
+      'map-overlay': MapOverlay
+    },
+    mounted(){
+      eventBus.$on('selected-user', (user) => {
+        this.user = user
+      })
+      eventBus.$on('log-out', () => this.user = null)
     }
-  },
-  components: {
-    'landing-page': LandingPage,
-    'profile-header': ProfileHeader,
-    'nav-bar': NavBar,
-    'map-overlay': MapOverlay
-  },
-  mounted(){
-    eventBus.$on('selected-user', (user) => {
-      this.user = user
-    })
-    eventBus.$on('log-out', () => this.user = null)
   }
-}
 </script>
 
 <style>
-* {
-  margin: 0;
-  padding: 0;
-}
+  * {
+    margin: 0;
+    padding: 0;
+  }
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-#profile-container {
-  display: grid;
-  grid-template-rows: 10vh;
-  grid-template-columns: 20vw auto;
-  grid-template-areas:
+  #profile-container {
+    display: grid;
+    grid-template-rows: 10vh;
+    grid-template-columns: 20vw auto;
+    grid-template-areas:
     'header header'
     'nav-bar map-overlay'
 
-}
+  }
 </style>
