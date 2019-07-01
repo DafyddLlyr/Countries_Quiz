8471765865523b387b7f77d076489b6ea882378b
Laura Sheach <laura_bubble_git@yahoo.com>
2019-07-01 15:51:45 +0100

    Questions, answers & complete styling done. Answer countered added to quiz complete page

--- client/src/components/MapOverlay.vue
+++ client/src/components/MapOverlay.vue
@@ -16,7 +16,7 @@
       <div id='quiz-container' v-if='selectedTopic'>
         <quiz-question v-if='(!answerSelected && questionCounter < 5)' :selectedTopic='selectedTopic' :user='user' :questionCounter="questionCounter"/>
         <quiz-answer v-if='(answerSelected && questionCounter < 5)' :questionPassed='questionPassed' :answerCountry='answerCountry' :questionCounter='questionCounter'/>
-        <quiz-complete v-if='quizCompleted' />
+        <quiz-complete v-if='quizCompleted' :correctAnswerCounter="correctAnswerCounter" />
       </div>
 
     </div>
@@ -44,6 +44,7 @@ export default {
       'questionCounter': 0,
       'quizCompleted': false,
       'quizChoice': false,
+      'correctAnswerCounter': 0,
       'mapDisplayCountry': null,
       'globe': null,
       'displayMarker': null,
@@ -82,6 +83,9 @@ export default {
       this.questionCounter += 1;
       this.quizCompleted = true;
     })
+    eventBus.$on('correct-answer', () => {
+      this.correctAnswerCounter +=1;
+    })
     eventBus.$on('quiz-reset', () => {
       this.selectedTopic = null;
       this.answerSelected = false;
@@ -89,6 +93,7 @@ export default {
       this.answerCountry = null;
       this.questionCounter = 0;
       this.quizCompleted = false;
+      this.correctAnswerCounter = 0;
     })
     eventBus.$on('country-choice', country => {
       this.mapDisplayCountry = country;
--- client/src/components/QuizAnswer.vue
+++ client/src/components/QuizAnswer.vue
@@ -1,10 +1,17 @@
 <template lang="html">
 
   <div id="quiz-answer">
+
     <quiz-progress :questionCounter="questionCounter + 1"/>
+
     <h2>{{displayAnswer()}}</h2>
-    <button v-on:click="handleNextQuestion" type="button" name="button">Next Question</button>
+
+    <div id="next-button">
+      <button  v-on:click="handleNextQuestion" type="button" name="button">Next Question</button>
+    </div>
+
   </div>
+
 
 </template>
 
@@ -43,11 +50,14 @@ export default {
   width: 70vw;
   min-height: 60vh;
   padding: 2vw;
-  background-color: yellow;
+  background-color: rgba(255, 255, 255, 0.75);
   color: black;
   display: flex;
   flex-direction: column;
-  align-content: space-between;
+  align-content: center;
+  font-size: 30px;
 }
+
+
 
 </style>
